df2b5d902fe5e5a785bd19969baefbbf371a1b99
yahya <yahyaest1000@gmail.com>
2023-09-17 00:48:26 +0100

    Finish comment api route

--- app/api/comment.py
+++ app/api/comment.py
@@ -1,7 +1,8 @@
 from typing import List, Optional
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel
 from app.prisma import prisma
+import json
 
 router = APIRouter()
 
@@ -11,6 +12,26 @@ class Comment(BaseModel):
     content: str
     threadId: int
 
+class UpdateComment(BaseModel):
+    userEmail: Optional[str] = None
+    userId: Optional[int] = None
+    content: Optional[str] = None
+    threadId: Optional[int] = None
+
+@router.get("/comments", tags=["comment"])
+async def getComments():
+    comments = await prisma.comment.find_many()
+    
+    return comments
+
+@router.get("/comments/{comment_id}", tags=["comment"])
+async def getComment(comment_id : int):
+    comment = await prisma.comment.find_unique(where={'id':comment_id })
+
+    if not comment:
+        raise HTTPException(status_code=404, detail="Comment not found")
+    
+    return comment
 
 @router.post("/comments", tags=["comment"])
 async def addComment(body: Comment):
@@ -24,8 +45,16 @@ async def addComment(body: Comment):
     )
     return comment
 
-@router.get("/comments", tags=["comment"])
-async def getComments():
-    comments = await prisma.comment.find_many()
+@router.patch("/comments/{comment_id}", tags=["comment"])
+async def updateComment(comment_id:int, body: dict):
+    comment = await prisma.comment.find_unique(where={'id': comment_id })
+
+    if not comment:
+        raise HTTPException(status_code=404, detail="Comment not found")
     
-    return comments
+    comment = await prisma.comment.update(
+        where= { 'id': comment_id },
+        data= dict(body)
+    )
+
+    return comment
