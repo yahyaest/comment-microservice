21783a3425b0d9bc316ad65a0d406ee892c7a950
yahya <yahyaest1000@gmail.com>
2023-09-21 01:14:47 +0100

    Implement Auth Middleware

--- app/main.py
+++ app/main.py
@@ -3,12 +3,16 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.prisma import prisma
 from app.api import api
+from app.middleware.auth import auth_middleware
 
 app = FastAPI()
 
+app.middleware("http")(auth_middleware)
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['http://localhost:3000'],
+    allow_origins=['*'],
+    allow_credentials=True,
     allow_methods=['*'],
     allow_headers=['*']
 )
