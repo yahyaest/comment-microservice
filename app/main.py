3b0594ab665e30745ba69433ddc9dfc8bc8b28fc
yahya <yahyaest1000@gmail.com>
2023-10-07 01:19:47 +0100

    Imlement jwt auth middleware

--- app/main.py
+++ app/main.py
@@ -1,13 +1,18 @@
 from typing import Union
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from starlette.middleware.authentication import AuthenticationMiddleware
 from app.prisma import prisma
 from app.api import api
-from app.middleware.auth import auth_middleware
+from app.middleware.jwt_auth import BearerAuthBackend
+from app.middleware.custom_auth import auth_middleware
 
 app = FastAPI()
 
-app.middleware("http")(auth_middleware)
+# app.middleware("http")(auth_middleware)
+
+
+app.add_middleware(AuthenticationMiddleware, backend=BearerAuthBackend())
 
 app.add_middleware(
     CORSMiddleware,
